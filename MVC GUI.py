a7c4922422d5b1e9c659593310efa3caea0e6e57
unknown <Mislav.stojanovic@gmail.com>
2022-01-18 10:11:02 +0100

    GUI with time limit control

--- MVC GUI.py
+++ MVC GUI.py
@@ -21,7 +21,8 @@ def createNeighborList(edges, num_nodes):
 
 class GeneticAlgorithm:
 
-    def __init__(self, E, W, population_size, n_gen, p_c, p_h, p_m, p_sc, p_better, p_u):
+    def __init__(self, E, W, population_size, n_gen, time_limit, p_c, p_h, p_m, p_sc,
+                 p_better, p_u):
         self.E = E
         self.W = W
 
@@ -36,16 +37,21 @@ class GeneticAlgorithm:
         self.p_sc = p_sc
         self.p_better = p_better
         self.p_u = p_u
+        self.time_limit = time_limit
 
     def run(self):
+
         population = self.generate_initial_population()
 
         current_best = self.find_best_solution(population)
+        genesis = 1
         WDR_list = [self.W[i] / self.degree(i) for i in range(len(self.E))]
         avg_WDR = sum(WDR_list) / len(self.E)
 
         gen = 0
         best_by_iteration = []
+        solution_geneses = [1]
+        stopwatch = time.time()
         while gen < self.n_gen:
             print("generation: ", gen)
             if random.random() < self.p_c:
@@ -53,8 +59,10 @@ class GeneticAlgorithm:
                 p2 = self.binary_tournament_selection(population)
 
                 new_solution = self.mutate(self.crossover(p1, p2), WDR_list, avg_WDR)
+                flag = 0
             else:
                 new_solution = self.generate_random_solution()
+                flag = 1
 
             new_solution = self.reduction(self.repair(new_solution))
 
@@ -65,10 +73,18 @@ class GeneticAlgorithm:
 
                 if self.calculate_fitness(new_solution) < self.calculate_fitness(current_best):
                     current_best = new_solution
+                    if(flag):
+                        genesis = 1
+                    else:
+                        genesis = 0
+                else: genesis = -1
 
                 best_by_iteration.append(current_best)
+                solution_geneses.append(genesis)
+            check_time = time.time()
+            if((check_time - stopwatch) > self.time_limit): break
 
-        return current_best, best_by_iteration
+        return current_best, best_by_iteration, solution_geneses
 
     def degree(self, x):
         return len(self.E[x])
@@ -390,34 +406,40 @@ class GeneticAlgorithm:
 
         return child
 
-def plot_results_by_iteration(best_by_iteration):
+def plot_results_by_iteration(best_by_iteration, solution_geneses):
     # TODO: add types of points on graph based on how the new best by iteration
     #   solution was created - crossover, random or something else
     fig, ax = plt.subplots()
-
+            
     ax.plot(range(len(best_by_iteration)), best_by_iteration, linewidth = 2.0)
 
+    for i in range(len(best_by_iteration)):
+        if(solution_geneses[i] == 1):
+            ax.plot(i, best_by_iteration[i], 'go', color = 'red')
+        elif(solution_geneses[i] == 0):
+            ax.plot(i, best_by_iteration[i], 'go', color = 'green')
+    print(solution_geneses)
     plt.show()
 
 
-def boom(filename, population_size, n_gen, random_weights):
+def boom(filename, population_size, n_gen, random_weights, time_limit):
 
     with open(filename, 'r') as input_file:
         W, edges = dimacs_random.readData(input_file, random_weights)
 
     E = createNeighborList(edges, len(W))
 
-    algorithm = GeneticAlgorithm(E, W, population_size, n_gen,
+    algorithm = GeneticAlgorithm(E, W, population_size, n_gen, time_limit, 
             p_c = 0.9, p_h = 0.2, p_m = 0.05, p_sc = 0.5,
             p_better = 0.8, p_u = 0.8)
 
-    solution, best_by_iteration = algorithm.run()
+    solution, best_by_iteration, solution_geneses = algorithm.run()
 
     print(algorithm.calculate_fitness(solution))
     print(algorithm.check_vertex_cover(solution))
 
     plot_results_by_iteration([algorithm.calculate_fitness(solution) for solution \
-            in best_by_iteration])
+            in best_by_iteration], solution_geneses)
 
 
 folder = 'datasets'
@@ -428,25 +450,27 @@ top = tk.Tk(className = 'Minimum vertex cover')
 f = tkf.Font(family='Helvetica', size=20, weight='bold')
 
 random_weights = tk.IntVar()
-filename = tk.StringVar()
-population_size = tk.IntVar()
-n_gen = tk.IntVar()
 
 w = tk.Checkbutton(top, text = "Generate random weights",
-                   font = '20', pady = 50, variable = random_weights)
+                   font = '20', pady = 30, variable = random_weights)
 lp = tk.Label(top, text = "Population:", font = '20',
               pady = 15)
 lg = tk.Label(top, text = "Generations:", font = '20',
               pady = 15)
+lt = tk.Label(top, text = "Time limit (seconds)", font = '20',
+              pady = 15)
 p = tk.Entry(top, exportselection = 0, justify = tk.CENTER)
 g = tk.Entry(top, exportselection = 0, justify = tk.CENTER)
+t = tk.Entry(top, exportselection = 0, justify = tk.CENTER)
 lm = tk.Label(top, text = 'Choose a graph', font = '20')
 optmenu = tkk.Combobox(top, values=filelist,
                        state='readonly')
 b = tk.Button(top, text = "Begin!", font = f,
               height = 20, width = 20, bg = 'black', fg = 'white',
               command = lambda: boom("datasets/" + optmenu.get(), int(p.get()),
-                                     int(g.get()), random_weights.get()))
+                                     int(g.get()) if len(g.get()) else float('inf'),
+                                     random_weights.get(),
+                                     int(t.get()) if len(t.get()) else float('inf')))
 
 lm.pack()
 optmenu.pack(fill='x')
@@ -454,7 +478,9 @@ lp.pack()
 p.pack()
 lg.pack()
 g.pack()
+lt.pack()
+t.pack()
 w.pack()
 b.pack(side = tk.BOTTOM)
-top.geometry('400x400')
+top.geometry('400x500')
 top.mainloop()
